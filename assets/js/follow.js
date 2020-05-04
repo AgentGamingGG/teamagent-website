442f13880d15e2b90176c8d5cd32edc3a2a3c764
twango <48845764+twitch-twango@users.noreply.github.com>
2020-05-04 11:45:28 -0700

    apply test

--- assets/js/follow.js
+++ assets/js/follow.js
@@ -1,8 +1,39 @@
 var twitterfollow = false;
 var instafollow = false;
-var tiktokfolllow = false;
+var tiktokfollow = false;
+var twitchfollow = false;
+var unlocked = 0;
+var amountunlock = 3;
 
 function twitterfollowclick() {
     var twitterfollow = true;
-    console.log("Twitter Unlocked")
+    console.log("Twitter Unlocked");
+    unlocked++;
+    if (unlocked == amountunlock) {
+        console.log("Unlocked")
+    }
+};
+function instafollowclick() {
+    var instafollow = true;
+    console.log("Instagram Unlocked");
+    unlocked++;
+    if (unlocked == amountunlock) {
+        console.log("Unlocked")
+    }
+};
+function tiktokfollowclick() {
+    var tiktokfollow = true;
+    console.log("TikTok Unlocked");
+    unlocked++;
+    if (unlocked == amountunlock) {
+        console.log("Unlocked")
+    }
+};
+function twitchfollowclick() {
+    var twitchfollow = true;
+    console.log("Twitch Unlocked");
+    unlocked++;
+    if (unlocked == amountunlock) {
+        console.log("Unlocked")
+    }
 };
