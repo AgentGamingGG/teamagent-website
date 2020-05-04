02a3a6e6ed01d31d0a1390a70882d26e57036926
twango <48845764+twitch-twango@users.noreply.github.com>
2020-05-03 20:45:35 -0700

    updates

---
+++ assets/js/follow.js
@@ -0,0 +1,8 @@
+var twitterfollow = false;
+var instafollow = false;
+var tiktokfolllow = false;
+
+function twitterfollowclick() {
+    var twitterfollow = true;
+    console.log("Twitter Unlocked")
+};
