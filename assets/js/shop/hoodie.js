1a37f515aa81ec037e638986915a3f7de19ac262
twango <48845764+twitch-twango@users.noreply.github.com>
2020-05-31 16:02:46 -0700

    hoodie

--- assets/js/shop/hoodie.js
+++ assets/js/shop/hoodie.js
@@ -13,3 +13,65 @@ function showDivs(n) {
     x[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " w3-opacity-off";
 }
+var small = document.getElementById("small");
+var medium = document.getElementById("medium");
+var large = document.getElementById("large");
+var xlarge = document.getElementById("xlarge");
+var xxlarge = document.getElementById("xxlarge");
+var selection = 0;
+var errormsg = document.getElementById('error');
+
+function clearall() {
+    small.style.borderColor = "rgb(138, 138, 138)";
+    medium.style.borderColor = "rgb(138, 138, 138)";
+    large.style.borderColor = "rgb(138, 138, 138)";
+    xlarge.style.borderColor = "rgb(138, 138, 138)";
+    xxlarge.style.borderColor = "rgb(138, 138, 138)";
+}
+function smallsize() {
+    selection = 1;
+    clearall();
+    small.style.borderColor = "white";
+}
+function mediumsize() {
+    selection = 2;
+    clearall();
+    medium.style.borderColor = "white";
+}
+function largesize() {
+    selection = 3;
+    clearall();
+    large.style.borderColor = "white";
+}
+function xlargesize() {
+    selection = 4;
+    clearall();
+    xlarge.style.borderColor = "white";
+}
+function xxlargesize() {
+    selection = 5;
+    clearall();
+    xxlarge.style.borderColor = "white";
+}
+function buynow() {
+    if (selection >= 1) {
+        if (selection == 1) {
+            window.open('https://agentgamingesports.com/checkout/hoodie/S');
+        }
+        if (selection == 2) {
+            window.open('https://agentgamingesports.com/checkout/hoodie/M');
+        }
+        if (selection == 3) {
+            window.open('https://agentgamingesports.com/checkout/hoodie/L');
+        }
+        if (selection == 4) {
+            window.open('https://agentgamingesports.com/checkout/hoodie/XL');
+        }
+        if (selection == 5) {
+            window.open('https://agentgamingesports.com/checkout/hoodie/XXL');
+        }
+    } else {
+        errormsg.style.display = "block";
+        console.log(selection);
+    }
+}
